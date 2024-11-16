1cc9c5480eaa9ed41f19d3bcd7e9f1a55d8e1137
TristanVoorjans <tristan.voorjans@alpinedigital.com>
2024-11-16 09:30:37 +0100

    Batman

---
+++ src/CookieNotice.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace voorjans\cookienotice;
+
+use voorjans\cookienotice\variables\CookieNoticeVariable;
+use craft\base\Plugin;
+use craft\web\twig\variables\CraftVariable;
+use yii\base\Event;
+
+class CookieNotice extends Plugin
+{
+    public function init()
+    {
+        parent::init();
+        Event::on(
+            CraftVariable::class,
+            CraftVariable::EVENT_INIT,
+            function (Event $event) {
+                /** @var CraftVariable $variable */
+                $var = $event->sender;
+                $var->set('cookieNotice', CookieNoticeVariable::class);
+            }
+        );
+    }
+}
